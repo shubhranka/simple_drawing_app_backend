2a538eaf52e6524dbfb5aecb8dfaa6aedab46130
shubhrankavarma@gmail.com <shubhrankavarma@gmail.com>
2024-12-15 13:41:44 +0530

    added lvels

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { GoogleGenerativeAI } from "@google/generative-ai";
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 
 //enable cors
 app.use((req, res, next) => {
@@ -19,24 +19,66 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 
 const model = genAI.getGenerativeModel({ model: 'models/gemini-1.5-flash' });
 
-const alreadyGivenThings = [];
+const alreadyGivenEasyThings = [];
+const alreadyGivenMediumThings = [];
+const alreadyGivenHardThings = [];
 
-app.get('/', async (req, res) => {
-  console.log("/");
+app.get('/easy', async (req, res) => {
   try{
     const result = await model.generateContent([
-      `Give some things that can be drawn simply by using mouse on a white board. Give simple not too hard to draw. Just give one thing. In the below format and do not print anything else:
+      `Give some things that can be drawn simply by using mouse on a white board. Give simple not too hard to draw. Dont not repeat the things you have already given. Just give one thing. In the below format and do not print anything else:
   
       Thing: {your thing}
   
       Things that you have already given:
-      {${alreadyGivenThings.join(',')}}
+      {${alreadyGivenEasyThings.join(',')}}
       `
     ])
   
-    const thing = result.response.text().split('Thing: ')[1].split('\n')[0];
-    alreadyGivenThings.push(thing);
+    const thing = result.response.text().split('Thing: ')[1].split('\n')[0].replace(/[^a-zA-Z0-9 ]/g, '');
+    alreadyGivenEasyThings.push(thing);
     res.send({ thing });
+  }catch(err){
+    res.status(429).send("Try Again Later");
+  }
+})
+
+app.get('/medium', async (req, res) => {
+  try{
+  const result = await model.generateContent([
+    `Give some things that can be drawn simply by using mouse on a white board. Give medium level thing neither too hard nor too easy to draw. Dont not repeat the things you have already given. Just give one thing. In the below format and do not print anything else:
+
+    Thing: {your thing}
+
+    Things that you have already given:
+    {${alreadyGivenMediumThings.join(',')}}
+    `
+  ])
+
+  const thing = result.response.text().split('Thing: ')[1].split('\n')[0].replace(/[^a-zA-Z0-9 ]/g, '');
+    alreadyGivenMediumThings.push(thing);
+    res.send({ thing });
+  }catch(err){
+    res.status(429).send("Try Again Later");
+  }
+})
+
+app.get('/hard', async (req, res) => {
+  try{
+    const result = await model.generateContent([
+      `Give some things that can be drawn simply by using mouse on a white board. Give hard level thing that is not easy to draw. Dont not repeat the things you have already given. Just give one thing. In the below format and do not print anything else:
+
+      Thing: {your thing}
+
+      Things that you have already given:
+      {${alreadyGivenHardThings.join(',')}}
+      `
+    ])
+
+    const thing = result.response.text().split('Thing: ')[1].split('\n')[0].replace(/[^a-zA-Z0-9 ]/g, '');
+    alreadyGivenHardThings.push(thing);
+    res.send({ thing });
+
   }catch(err){
     res.status(429).send("Try Again Later");
   }
@@ -55,7 +97,6 @@ app.post('/check', async (req, res) => {
     `Is it a sketch of a ${thing}? just answer "yes" or "no."`,
 ]);
 const response = result.response.text()
-console.log(response);
 if (response.toLowerCase().includes('yes')) {
   res.send(true);
 } else {
